d13396eb4eee8484dc9e95324a0acf770fc985cd
Sean <seanwalkerb+github@pm.me>
2026-07-04 14:23:34 +1000

    updates to nvim and ghostty

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,12 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
 	config = function()
-		vim.cmd("colorscheme rose-pine-moon")
+		vim.cmd.colorscheme("tokyonight-night")
 	end,
 }
-
---[[return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    vim.cmd.colorscheme("tokyonight-night")
-  end,
-}
-]]
 
 --[[return {
   dir = vim.fn.stdpath("config") .. "/lua/colors",
